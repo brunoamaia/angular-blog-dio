59f8746bdbb90a8abe52789909344e0fdee63c6d
brunoamaia <brunoamaia94@gmail.com>
2023-04-25 12:27:59 -0300

    remoção do nx console

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { MenuBarComponent } from './component/menu-bar/menu-bar.component';
 import { SmallCardComponent } from './component/small-card/small-card.component';
 import { BigCardComponent } from './component/big-card/big-card.component';
+import { MenuTitleComponent } from './component/menu-title/menu-title.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuBarComponent,
     SmallCardComponent,
-    BigCardComponent],
+    BigCardComponent,
+    MenuTitleComponent],
   imports: [
     BrowserModule,
     AppRoutingModule
--- src/app/component/big-card/big-card.component.ts
+++ src/app/component/big-card/big-card.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
-  styleUrls: ['./big-card.component.css']
+  styleUrls: ['./big-card.component.scss']
 })
 export class BigCardComponent {}
