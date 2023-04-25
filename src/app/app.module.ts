7b61c9553519d2d4c39e1331ba3bf893761b4dcf
brunoamaia <brunoamaia94@gmail.com>
2023-04-25 14:01:19 -0300

    Renomeação de component para components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MenuBarComponent } from './component/menu-bar/menu-bar.component';
-import { SmallCardComponent } from './component/small-card/small-card.component';
-import { BigCardComponent } from './component/big-card/big-card.component';
-import { MenuTitleComponent } from './component/menu-title/menu-title.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
 
 @NgModule({
   declarations: [
